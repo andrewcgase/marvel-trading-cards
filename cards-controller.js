b5b73a51089f7857d8549a69b3c7a8105272c8f0
andrewgase <andrewgase@gmail.com>
2017-01-30 16:14:11 -0700

    Fine!

--- cards-controller.js
+++ cards-controller.js
@@ -4,62 +4,76 @@ function CardsController() {
   var dataStore = new MarvelService()
 
 
+  console.log(dataStore)
+
   dataStore.getCharacters(ready)
+  console.log(dataStore)
+
+  
+
+
+  this.onAdd = function (charId) {
+    //this function will take the player that was clicked and add them to your team through the dataStore.
+    dataStore.addToMyCharacters(charId)
+    draw(dataStore.getMarvelCharacters(),dataStore.getMyCharacters())
+  }
+
+  this.onRemove = function (charId) {
+    //this will remove the character from your team
+    dataStore.removeMyCharacter(charId)
+    draw(dataStore.getMarvelCharacters(),dataStore.getMyCharacters())
+  }
+
 
   function ready(data) {
+    draw(data, [])
+  }
 
-    draw(data)
 
-    this.onAdd = function () {
-      //this function will take the player that was clicked and add them to your team through the dataStore.
 
+
+  function draw(marvelList, myList) {
+    //target is the id of the element where the list will be rendered
+    var marvelElem = document.getElementById('marvel-characters')
+    var myElem = document.getElementById('my-characters')
+    marvelElem.innerHTML = ''
+    myElem.innerHTML = ''
+
+    var marvelTemplate = ''
+    var myTemplate = ''
+
+    for (var i in marvelList) {
+      var character = marvelList[i];
+      marvelTemplate += `
+          <div class="card">
+            <img src="${character.thumbnail.path}.${character.thumbnail.extension}" width="100">
+            <h3>${character.name}</h3>
+            <div>
+              <button class="btn-success" id="${character.id}" onclick="cardsCtrl.onAdd(${character.id})">Add to Team</button>
+            </div>
+          <div>
+        `
     }
 
-    this.onRemove = function () {
-      //this will remove the character from your team
+    for (var i in myList) {
+      var character = myList[i];
+      myTemplate += `
+          <div class="card">
+            <img src="${character.thumbnail.path}.${character.thumbnail.extension}" width="100">
+            <h3>${character.name}</h3>
+            <div>
+              <button class="btn-danger" id="${character.id}" onclick="cardsCtrl.onRemove(${character.id})">DESTROY FOREVER</button>
+            </div>
+          <div>
+          `
     }
 
-
-    function draw(list) {
-      //target is the id of the element where the list will be rendered
-      var marvelElem = document.getElementById('marvel-characters')
-      var myElem = document.getElementById('my-characters')
-      marvelElem.innerHTML = ''
-      myElem.innerHTML = ''
-
-      var marvelTemplate = ''
-      var myTemplate = ''
-
-      for (var i in list) {
-        console.log(list)
-        var character = list[i];
-        console.log(character);
-        marvelTemplate += `
-      <div class="card">
-        <img src="${character.thumbnail.path}.${character.thumbnail.extension}" width="100">
-        <h3>${character.name}</h3>
-        <div>
-          <button class="btn-success" id="${character.id}">Add to Team</button>
-        </div>
-      <div>
-      `
-        myTemplate += `
-      <div class="card">
-        <img src="${character.thumbnail.path}.${character.thumbnail.extension}" width="100">
-        <h3>${character.name}</h3>
-        <div>
-          <button class="btn-danger" id="${character.id}">DESTROY FOREVER</button>
-        </div>
-      <div>
-      `
-      }
-      
-      marvelElem.innerHTML = marvelTemplate
-      myElem.innerHTML = myTemplate;
-
-    }
+    marvelElem.innerHTML = marvelTemplate
+    myElem.innerHTML = myTemplate;
 
   }
+
+
 
 
 }
--- marvel-service.js
+++ marvel-service.js
@@ -8,20 +8,41 @@ function MarvelService(){
   
   this.getMarvelCharacters = function(){
     //what should this function return
+    return marvelCharacters.slice(0,marvelCharacters.length)
   }
   
   this.getMyCharacters = function(){
     //what should this function return
+    return myCharacters.slice(0,myCharacters.length)
   }
   
   this.addToMyCharacters = function(id){
     //in order to add a character to your list you will first need to find 
     //the character by its id in the marvelCharacters array
+    for(var i =0; i<marvelCharacters.length ; i++){
+      var currentCharacter = marvelCharacters[i]
+      if(id == currentCharacter.id){
+        marvelCharacters.splice(i,1)
+        myCharacters.push(currentCharacter)
+        console.log(myCharacters)
+        return
+      }
+      
+    }
+  
   }
   
   this.removeMyCharacter = function(id){
     //you need to find the character that you want to remove by its id
     //and remove it.
+    for(var i = 0; i<myCharacters.length; i++){
+      var currentCharacter = myCharacters[i]
+      if(id == currentCharacter.id){
+        myCharacters.splice(i,1)
+        marvelCharacters.push(currentCharacter)
+        return
+      }
+    }
   }
   
   
